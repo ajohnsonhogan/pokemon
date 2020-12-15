3d51d8b19a58b6f2280770e79be9f2367b11d4b5
Asher Johnson Hogan <ajohnsonhogan@ufl.edu>
2020-12-14 20:08:57 -0500

    Asher: added PCA and graph of mean stats per type

--- correlation.py
+++ correlation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas.plotting import parallel_coordinates
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,4 +22,16 @@ plt.figure(figsize=(9, 9))
 corr = pokemon.corr()
 corr_lt = corr.where(np.triu(np.ones(corr.shape)).astype(np.bool).__invert__())
 sns.heatmap(corr_lt, cmap="vlag", xticklabels=True, yticklabels=True)
+plt.show()
+
+# grouped stats line graph
+pokemon["type1"] = pokemon.apply(lambda row: row.type1.name, axis=1)
+grouped = pokemon.groupby(["type1"]).mean().reset_index()
+plt.figure(figsize=(8, 4))
+parallel_coordinates(grouped[
+        ["type1", "attack", "defense", "hp", "sp_attack", "sp_defense", "speed"]
+    ], "type1")
+ax = plt.gca()
+handles, labels = ax.get_legend_handles_labels()
+ax.legend(handles, labels, loc='upper right', bbox_to_anchor=(1.3, 1))
 plt.show()
---
+++ pca.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from type import Type
+
+with open('pokemon.csv') as p:
+    pokemon = pd.read_csv(p)
+
+features = ["base_total", "attack", "defense", "hp", "sp_attack", "sp_defense", "speed", "base_egg_steps",
+            "base_happiness", "capture_rate", "experience_growth", "height_m", "weight_kg", "generation", "is_legendary"
+            ]
+
+x = pokemon.loc[:, features].values
+x = StandardScaler().fit_transform(x)
+y = pokemon.loc[:, ["type1"]].values
+
+pca = PCA()
+pca.fit_transform(x)
+
+# plots explained variance for each principal component
+plt.plot(pca.explained_variance_)
+plt.ylabel("explained variance")
+plt.xlabel("principal component")
+plt.show()
+
+# with top 2 principal components...
+pca = PCA(n_components=2)
+principal_components = pca.fit_transform(x)
+
+principalDf = pd.DataFrame(principal_components, columns=['principal component 1', 'principal component 2'])
+finalDf = pd.concat([principalDf, pokemon[['type1']]], axis=1)
+finalDf["type1"] = finalDf.apply(lambda row: Type[row.type1.upper()].value, axis=1)
+
+# scatter plot of each pokemon according to type based on the top 2 principal components
+finalDf.plot.scatter(x="principal component 1",
+                     y="principal component 2",
+                     c="type1",
+                     colormap="tab20c",
+                     colorbar=False)
+plt.show()
